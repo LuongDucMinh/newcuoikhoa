cb0f9cea1ecc5418f723cf972583c55e5f3cbf93
LuongDucMinh <ducminhlc280702@gmail.com>
2022-12-03 00:54:09 +0700

    shopping

---
+++ shopping-cart-js-starter-files/src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Gimbap",
+    price: 35,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/gimbab.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Salmon Shushi",
+    price: 60,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/shushi.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "Shushi Egg",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/shushiegg.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Shushi Set nigiri",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/shushiset.jpg",
+  },
+];
